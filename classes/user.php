7909cb7b41ed40b75fd43c3818fd28133695223f
Grumbledore667 <nirvanafs@googlemail.com>
2017-06-03 18:06:31 +0200

    Add files via upload
    User.php --> Bug fix

--- classes/user.php
+++ classes/user.php
@@ -242,7 +242,7 @@ class USER
         return $success;
     }
     
-    public function setExpireDate($id){
+    public function ExpireDate($id){
         $expdate = date('Y-m-d', strtotime("-300 days"));
         $stmt = $this->db->prepare("UPDATE `Users` SET `ExpireDate`=:expdate WHERE `ID`=:id");
         $success = $stmt->execute(array(":id"=>$id, ":expdate"=>$expdate));
